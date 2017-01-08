88ad78405b24822f0cb5dc93fabe3bebe78cc104
Froks15 <frokstv@gmail.com>
2017-01-08 12:30:40 +0400

    v1.1

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php include_once "connect.php" ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +9,31 @@
 	<script src="main.js"></script>
 </head>
 <body>
+
+<form method="post">
+
+	<input type="text" name="login" placeholder="Введите логин"> <br>
+	<input type="password" name="password" placeholder="Введите пароль"> <br>
+	<input type="email" name="email" placeholder="Введите почту"> <br>
+	<input type="submit" value="Зарегестрироваться" name="reg">
+
+</form>
+
+
+<?php
+
+if (isset($_POST['reg'])) {
+
+	$login = $_POST['login'];
+	$password = $_POST['password'];
+	$email = $_POST['email'];
 	
+}
+
+
+
+
+?>
+
 </body>
 </html>
